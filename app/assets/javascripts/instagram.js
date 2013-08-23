71be565b441c84ba117ac8975a0ac77018a1c5d8
Clay Miller <clay@smockle.com>
2013-08-23 17:40:08 -0500

    Enable team buttons.

--- app/assets/javascripts/instagram.js
+++ app/assets/javascripts/instagram.js
@@ -11,6 +11,41 @@ COUCH = $.extend(typeof COUCH === "undefined" ? {} : COUCH, {
           $("section#feeds").html(data);
         }
       });
+      
+      // Show all teams.
+      $("#link-all").on("click", function() {
+        $(".menu-link").removeClass("menu-link--active");
+        $("#link-all").addClass("menu-link--active");
+        $(".media").show();
+      });
+      
+      // Show first team.
+      $("#link-one").on("click", function() {
+        $(".menu-link").removeClass("menu-link--active");
+        $("#link-one").addClass("menu-link--active");
+        $(".media:not(.media-one)").hide();
+      });
+      
+      // Show second team.
+      $("#link-two").on("click", function() {
+        $(".menu-link").removeClass("menu-link--active");
+        $("#link-two").addClass("menu-link--active");
+        $(".media:not(.media-two)").hide();
+      });
+      
+      // Show third team.
+      $("#link-three").on("click", function() {
+        $(".menu-link").removeClass("menu-link--active");
+        $("#link-three").addClass("menu-link--active");
+        $(".media:not(.media-three)").hide();
+      });
+      
+      // Show fourth team.
+      $("#link-four").on("click", function() {
+        $(".menu-link").removeClass("menu-link--active");
+        $("#link-four").addClass("menu-link--active");
+        $(".media:not(.media-four)").hide();
+      });
     }
   }
 });
