04d0f10ee59a17ed4a425b14675da95b812aee82
Clay Miller <clay@smockle.com>
2013-08-23 22:03:06 -0500

    Get more than 20 Instagram pictures.

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -7,7 +7,8 @@ class Feed
     Rails.cache.fetch("instagram", :expires_in => 60.seconds) do
       HTTParty.get("https://api.instagram.com/v1/tags/aumccollege/media/recent",
       :query => {
-        :access_token => Figaro.env.INSTAGRAM_ACCESS_TOKEN
+        :access_token => Figaro.env.INSTAGRAM_ACCESS_TOKEN,
+        :count => -1
       })["data"]
       # reject{|i| i["tags"].include? "p"}.first["images"]["standard_resolution"]["url"]
     end
