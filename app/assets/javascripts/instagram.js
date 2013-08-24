a7e29fed975475f5c1f1cf17c9dbc67bf25b030f
Clay Miller <clay@smockle.com>
2013-08-23 19:10:45 -0500

    DRYed JavaScript. Reload feed every thirty seconds.

--- app/assets/javascripts/instagram.js
+++ app/assets/javascripts/instagram.js
@@ -1,6 +1,40 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 
+function linkAll() {
+  $(".menu-link").removeClass("menu-link--active");
+  $("#link-all").addClass("menu-link--active");
+  $(".media").show();
+}
+
+function linkOne() {
+  $(".menu-link").removeClass("menu-link--active");
+  $("#link-one").addClass("menu-link--active");
+  $(".media").hide();
+  $(".media-one").show();
+}
+
+function linkTwo() {
+  $(".menu-link").removeClass("menu-link--active");
+  $("#link-two").addClass("menu-link--active");
+  $(".media").hide();
+  $(".media-two").show();
+}
+
+function linkThree() {
+  $(".menu-link").removeClass("menu-link--active");
+  $("#link-three").addClass("menu-link--active");
+  $(".media").hide();
+  $(".media-three").show();
+}
+
+function linkFour() {
+  $(".menu-link").removeClass("menu-link--active");
+  $("#link-four").addClass("menu-link--active");
+  $(".media").hide();
+  $(".media-four").show();
+}
+
 COUCH = $.extend(typeof COUCH === "undefined" ? {} : COUCH, {
   instagram: {
     index: function() {
@@ -12,44 +46,27 @@ COUCH = $.extend(typeof COUCH === "undefined" ? {} : COUCH, {
         }
       });
       
-      // Show all teams.
-      $("#link-all").on("click", function() {
-        $(".menu-link").removeClass("menu-link--active");
-        $("#link-all").addClass("menu-link--active");
-        $(".media").show();
-      });
+      // Reload feeds every thirty seconds.
+      setInterval(function() {
+        $.ajax({
+          url: "/instagram/feed",
+          success: function(data) {
+            console.log("Loaded images.");
+            $("section#feeds").html(data);
+            if ($("#link-all").hasClass("menu-link--active")) linkAll();
+            else if ($("#link-one").hasClass("menu-link--active")) linkOne();
+            else if ($("#link-two").hasClass("menu-link--active")) linkTwo();
+            else if ($("#link-three").hasClass("menu-link--active")) linkThree();
+            else if ($("#link-four").hasClass("menu-link--active")) linkFour();
+          }
+        });
+      }, 30000);
       
-      // Show first team.
-      $("#link-one").on("click", function() {
-        $(".menu-link").removeClass("menu-link--active");
-        $("#link-one").addClass("menu-link--active");
-        $(".media").hide();
-        $(".media-one").show();
-      });
-      
-      // Show second team.
-      $("#link-two").on("click", function() {
-        $(".menu-link").removeClass("menu-link--active");
-        $("#link-two").addClass("menu-link--active");
-        $(".media").hide();
-        $(".media-two").show();
-      });
-      
-      // Show third team.
-      $("#link-three").on("click", function() {
-        $(".menu-link").removeClass("menu-link--active");
-        $("#link-three").addClass("menu-link--active");
-        $(".media").hide();
-        $(".media-three").show();
-      });
-      
-      // Show fourth team.
-      $("#link-four").on("click", function() {
-        $(".menu-link").removeClass("menu-link--active");
-        $("#link-four").addClass("menu-link--active");
-        $(".media").hide();
-        $(".media-four").show();
-      });
+      $("#link-all").on("click", linkAll);
+      $("#link-one").on("click", linkOne);
+      $("#link-two").on("click", linkTwo);
+      $("#link-three").on("click", linkThree);
+      $("#link-four").on("click", linkFour);
     }
   }
 });
