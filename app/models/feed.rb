645bfc40b7401577a51cada38e5b77829c237f89
Clay Miller <clay@smockle.com>
2013-08-23 19:12:01 -0500

    Reduced cache time to 60 seconds.

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -4,7 +4,7 @@ class Feed
   include HTTParty
   
   def self.instagram
-    Rails.cache.fetch("instagram", :expires_in => 10.minutes) do
+    Rails.cache.fetch("instagram", :expires_in => 60.seconds) do
       HTTParty.get("https://api.instagram.com/v1/tags/aumccollege/media/recent",
       :query => {
         :access_token => Figaro.env.INSTAGRAM_ACCESS_TOKEN
